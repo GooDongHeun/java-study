636dc017fb41dcdb812c28c74df45a91b7673fe3
GooDongHeun <cool485@naver.com>
2019-02-22 10:49:18 +0900

    2/22

---
+++ src/collection/list/SearchList2.java
@@ -0,0 +1,40 @@
+package collection.list;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SearchList2 {
+	private static String strNum;
+	private static int num;
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numList = new ArrayList<>();
+		for(int i=0;i<5;i++) {
+			int ranNum = (int)(Math.random()*20)+1;
+					numList.add(ranNum);
+		}
+		Scanner scan = new Scanner(System.in);
+		System.out.println("맞출번호를 ,를 기준으로 입력해주세요.");
+		String strNum = scan.nextLine();
+		String[] strNums = strNum.split(",");
+		ArrayList<Integer> anList = new ArrayList<>();
+		for(int i=0;i<strNums.length;i++) {
+			int num = Integer.parseInt(strNums[i]);
+			//Integer.parseInt = String을 int로 변환함
+			anList.add(num);
+			
+	}
+		int nums = 0;
+		for(int i=0;i<anList.size();i++) {
+		int anNum = anList.get(i);
+		if(numList.indexOf(anNum)!=-1) {
+			num++;
+		}
+		System.out.println("numList : " + numList);
+		System.out.println("anList : " + anList);
+		System.out.println("맞춘 갯수 : " + num);
+	}
+
+	}
+}
+
